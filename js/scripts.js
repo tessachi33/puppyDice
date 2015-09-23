aa253e2266a0db77db0e22bf622d050ec55e3e89
Haley Harrison & Juliana Suzuki <student@epicodus.com>
2015-09-23 16:05:15 -0700

    player score resets to 0 correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 //global variables
 var player1 = 0;
 var player2 = 0;
+var finalScore1 = 0;
+var finalScore2 = 0;
 
 var diceRoll =  function() {
   return (Math.floor(Math.random() * 6 + 1));
@@ -12,7 +14,12 @@ var turn = function(player) {
 
   if (diceResult === 1) {
     tempScore = 0;
+    player1 = 0;
+    player2 = 0;
     alert("Boo! You lost your turn because you rolled a 1.");
+    return tempScore;
+  } else if (tempScore >= 100) {
+    alert("You win, great, blah.");
     return tempScore;
   } else {
     tempScore = tempScore + diceResult;
@@ -30,13 +37,11 @@ var turn = function(player) {
 
 $(document).ready(function() {
 
-  var player1score = player1;
-  var player2score = player2;
   var p1tempScore = 0;
   var p2tempScore = 0;
 
-  $(".player1score").text(player1score);
-  $(".player2score").text(player2score);
+  $(".player1score").text(player1);
+  $(".player2score").text(player2);
 
   $("form#rollDice1").submit(function(event) {
     var tempScore = turn(player1);
